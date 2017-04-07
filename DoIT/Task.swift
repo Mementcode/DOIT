848b6982be72db859354ff8cd51f1cde7943a64f
callum brennan <callum.brennan@hotmail.co.uk>
2017-04-08 00:00:30 +0100

    Created first view controller - Class named Tasks with 2 objects in function that uses a boolean type to decide if something is important or not.

---
+++ DoIT/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIT
+//
+//  Created by callum brennan on 07/04/2017.
+//  Copyright © 2017 callum brennan Mementcode. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    
+    var name = ""
+    var important = false
+}
--- DoIT/ViewController.swift
+++ DoIT/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+        
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
 }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "‼️\(task.name)"
+        }else {
+            cell.textLabel?.text = task.name
+        }
+
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        
+        let task1 = Task()
+        task1.name = "Walk Luna"
+        task1.important = false
+
+        
+        let task2 = Task()
+        task2.name = "Go Shopping"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "make DOIT app"
+        task3.important = true
+        
+        return [ task1,task2,task3]
+        
+    }
+    
+}
+
 
